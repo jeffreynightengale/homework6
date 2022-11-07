1c31795f9ba59f51becc3c3226e6a266d981a0a6
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-07 16:43:54 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,21 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 4</title>
+  </head>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="">Navbar</a>
+    <a class="navbar-brand" href="location.php">Location</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">D3.js</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Light Bulb</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="hide.php">Hide Text</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="change.php">Change Text</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="alert.php">Alert</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+<body>
