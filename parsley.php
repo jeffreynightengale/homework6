741fd905007df1c5ce3e1ce0f43a195aa530eaee
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-09 13:45:34 -0600

    Update parsley.php

--- parsley.php
+++ parsley.php
@@ -1,67 +1,46 @@
-<div class="bs-callout bs-callout-warning hidden">
-  <h4>Oh snap!</h4>
-  <p>This form seems to be invalid :(</p>
-</div>
+<?php require_once("header.php"); ?>
+<!DOCTYPE html>
+<html>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
-<div class="bs-callout bs-callout-info hidden">
-  <h4>Yay!</h4>
-  <p>Everything seems to be ok :)</p>
-</div>
+<body>
+<div id="myPlot" style="width:100%;max-width:700px"></div>
 
-<form id="demo-form" data-parsley-validate="">
-  <label for="fullname">Full Name * :</label>
-  <input type="text" class="form-control" name="fullname" required="">
+<script>
+var exp1 = "x";
+var exp2 = "1.5*x";
+var exp3 = "1.5*x + 7";
+// Generate values
 
-  <label for="email">Email * :</label>
-  <input type="email" class="form-control" name="email" data-parsley-trigger="change" required="">
+var x1Values = [];
+var x2Values = [];
+var x3Values = [];
+var y1Values = [];
+var y2Values = [];
+var y3Values = [];
 
-  <label for="contactMethod">Preferred Contact Method *:</label>
-  <p>
-    Email: <input type="radio" name="contactMethod" id="contactMethodEmail" value="Email" required="">
-    Phone: <input type="radio" name="contactMethod" id="contactMethodPhone" value="Phone">
-  </p>
+for (var x = 0; x <= 10; x += 1) {
+  x1Values.push(x);
+  x2Values.push(x);
+  x3Values.push(x);
+  y1Values.push(eval(exp1));
+  y2Values.push(eval(exp2));
+  y3Values.push(eval(exp3));
+}
 
-  <label for="hobbies">Hobbies (Optional, but 2 minimum):</label>
-  <p>
-    Skiing <input type="checkbox" name="hobbies[]" id="hobby1" value="ski" data-parsley-mincheck="2"><br>
-    Running <input type="checkbox" name="hobbies[]" id="hobby2" value="run"><br>
-    Eating <input type="checkbox" name="hobbies[]" id="hobby3" value="eat"><br>
-    Sleeping <input type="checkbox" name="hobbies[]" id="hobby4" value="sleep"><br>
-    Reading <input type="checkbox" name="hobbies[]" id="hobby5" value="read"><br>
-    Coding <input type="checkbox" name="hobbies[]" id="hobby6" value="code"><br>
-  </p>
+// Define Data
+var data = [
+  {x: x1Values, y: y1Values, mode:"lines"},
+  {x: x2Values, y: y2Values, mode:"lines"},
+  {x: x3Values, y: y3Values, mode:"lines"}
+];
 
-  <p>
-  <label for="heard">Heard about us via *:</label>
-  <select id="heard" required="">
-    <option value="">Choose..</option>
-    <option value="press">Press</option>
-    <option value="net">Internet</option>
-    <option value="mouth">Word of mouth</option>
-    <option value="other">Other..</option>
-  </select>
-  </p>
+//Define Layout
+var layout = {title: "[y=" + exp1 + "]  [y=" + exp2 + "]  [y=" + exp3 + "]"};
 
-  <p>
-  <label for="message">Message (20 chars min, 100 max) :</label>
-  <textarea id="message" class="form-control" name="message" data-parsley-trigger="keyup" data-parsley-minlength="20" data-parsley-maxlength="100" data-parsley-minlength-message="Come on! You need to enter at least a 20 character comment.." data-parsley-validation-threshold="10"></textarea>
-  </p>
-
-  <br>
-  <input type="submit" class="btn btn-default" value="validate">
-
-  <p><small>* Please, note that this demo form is not a perfect example of UX-awareness. The aim here is to show a quick overview of parsley-API and Parsley customizable behavior.</small></p>
-</form>
-
-<script type="text/javascript">
-$(function () {
-  $('#demo-form').parsley().on('field:validated', function() {
-    var ok = $('.parsley-error').length === 0;
-    $('.bs-callout-info').toggleClass('hidden', !ok);
-    $('.bs-callout-warning').toggleClass('hidden', ok);
-  })
-  .on('form:submit', function() {
-    return false; // Don't submit form for this demo
-  });
-});
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
 </script>
+
+</body>
+</html>
